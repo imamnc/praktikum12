7f5ed39f1f7547f7808657ac7aba09ccfa60d2c9
Imam Nc <imamnurcholis589@gmail.com>
2020-04-30 16:01:49 +0700

    First Commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# MDC-102 for Material Components for Android (Kotlin)
+
+Contains starter code structure for the MDC-102 Kotlin codelab.
+Praktikum 12
---
+++ app/src/main/java/com/example/shrine/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.shrine
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.codelabs.mdc.kotlin.shrine.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
